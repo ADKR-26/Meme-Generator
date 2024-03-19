52724c8ecb5720b0b281cb7b13447aad6bbe2a6e
Aaditya Kumar <adityavishwakarma2011@gmail.com>
2024-03-19 13:50:28 +0530

    Added Visit API

--- src/components/VisitCount.jsx
+++ src/components/VisitCount.jsx
@@ -5,6 +5,36 @@ const VisitCount = () => {
 
     const [visitData, setVisitData] = useState(0);
 
+    const [location, setLocation] = useState(null);
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    const lat = position.coords.latitude;
+                    const lng = position.coords.longitude;
+                    fetch(
+                        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=YOUR_API_KEY`
+                    )
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.status === "OK") {
+                                const place = data.results[0].formatted_address;
+                                setLocation(place);
+                            } else {
+                                setLocation("Could not retrieve place information.");
+                            }
+                        });
+                },
+                error => {
+                    setLocation(`Error: ${error.message}`);
+                }
+            );
+        } else {
+            setLocation("Geolocation is not supported by this browser.");
+        }
+    }, []);
+
     const postData = async () => {
         try {
             const response = await axios.post('https://counter-api.onrender.com/counter', {
@@ -25,6 +55,8 @@ const VisitCount = () => {
             <div style={{ color: 'white', padding: '1rem' }}>
                 <h2 style={{ fontSize: '1rem' }}>Visit Count: {visitData}</h2>
                 {/* <p style={{ fontSize: '1rem', fontWeight: 'bold' }}>{visitData}</p> */}
+                <h1>Your current location is:</h1>
+                <p>{location}</p>
             </div>
         </div>
     )
